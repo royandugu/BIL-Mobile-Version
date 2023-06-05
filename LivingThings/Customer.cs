d9d6da8cd12a3bb5892484d9fe973a6bfde1ea34
Royan Dugu <royandugu99009@gmail.com>
2023-06-05 20:35:00 +0545

    Customer generation

--- LivingThings/Customer.cs
+++ LivingThings/Customer.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
 public class Customer : MonoBehaviour {
     [SerializeField]
-    
+    private Transform limit,startPoint;
 
+    private void Start(){
+        transform.position=startPoint.position;
+    }
     private void Update() {
-        /*Go left till a certain point and go upwards , if (a certain distance already has 
-        an another customer waiting, they stand behind the customer and the value kati paxadi basne depends upon ShopStats.currentCustomerCount)
-        */
-        transform.position+=new Vector3(0,1,0)*Time.deltaTime*3;
+        if(transform.position.y<=limit.position.y-ShopStats.currentCustomerCount){
+            transform.position+=new Vector3(0,1,0)*Time.deltaTime*3;
+        }
     }
 }
